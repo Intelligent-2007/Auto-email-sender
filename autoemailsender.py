4290d5431e7aa430066d4b61f5e92bb1a24dbee7
Intelligent-2007 <85723710+Intelligent-2007@users.noreply.github.com>
2021-06-14 14:32:33 +0530

    Update autoemailsender.py

--- autoemailsender.py
+++ autoemailsender.py
@@ -1,66 +1,67 @@
-import smtplib as smt 
-import urllib.request
-from time import sleep
+try:
+    import smtplib as smt               
+    import urllib.request                
+    from time import sleep            
 
-# check for internet conncetion
-def connect(host='http://google.com'):
-    try:
-        urllib.request.urlopen(host)
-        return True
-    except:
-        return False
+    # check for internet conncetion
+    def connect(host='http://google.com'):
+        try:
+            urllib.request.urlopen(host)
+            return True
+        except:
+            return False
 
-# check for document made
-def egmail(file = "Registered_Emails"):
-    try:
-        f = open(file,"r")
-        return True
-    except:
-        return False
+    # check for document made
+    def egmail(file = "Registered_Emails"):
+        try:
+            f = open(file,"r")
+            return True
+        except:
+            return False
 
 
-# Add email and register email
-while True:
-    Made = egmail()
+    # Add email and register email
+    while True:
+        Made = egmail()
 
-    if Made == True:
-        print("Hello Welocme to the email bot")
-        print("Do you want to send email or add email in your registerd emails")
-        a = input()
-        if a == "Send email":
-             break
-        if a == "Add email":
+        if Made == True:                                                                               
+            print("Hello Welocme to the email bot")
+            print("Do you want to send email or add email in your registerd emails")
+            a = input()
+            if a == "Send email":
+                 break                                                                        
+            if a == "Add email":                                                          
+                print("Senders Email:- ")   
+                email = input()
+                print("Senders Password:- ")
+                password = input()
+                with open("Registered_Emails","a") as f :                        
+                    f.write(f"{email} \n ")
+                    f.write(f"{password} \n")
+                    quit()
+   
+        if Made == False:
             print("Senders Email:- ")   
             email = input()
             print("Senders Password:- ")
             password = input()
             with open("Registered_Emails","a") as f :
-             f.write(f"{email} \n ")
-             f.write(f"{password} \n")
-             quit()
-   
-    if Made == False:
-        print("Senders Email:- ")   
-        email = input()
-        print("Senders Password:- ")
-        password = input()
-        with open("Registered_Emails","a") as f :
-            f.write(f"{email} \n ")
-            f.write(f"{password} \n")
+                f.write(f"{email} \n ")
+                f.write(f"{password} \n")
             
-        print(f"sucessfully regestired {email}")
-        continue
-    break          
-# Variable defined take user input
+            print(f"sucessfully regestired {email}")
+            continue
+            break          
+    # Variable defined take user input
 
 
-Internet = connect()
-print("Write the Gmail On which u want to send message")
-Reciver = input()
-print("Now write down the messsage which u want to send")
-Message = input()
+    Internet = connect()
+    print("Write the Gmail On which u want to send message")
+    Reciver = input()
+    print("Now write down the messsage which u want to send")
+    Message = input()
 
-while True:
+    while True:
         Internet = connect()
         if Internet == True:
             print("sucessfully connected to internet")
@@ -70,7 +71,7 @@ while True:
             file.close()
 
             print(details)    
-            print("Type the email line number which u want")
+            print("Type the email number which u want")
             number = int(input())
             
             Number1 = number - 2
@@ -107,9 +108,23 @@ while True:
                     Internet = connect()
                     sleep(1)
 
+except:
+    print("Something went wrong")
+
+# Code written by intelligent2007
+# Reviewed and helped by Gamingfever8250
+             
+             
+            
 
 
-# Code return by intelligent2007 and Gamingfever8250
+
+
+
+
+
+
+
 
              
              
