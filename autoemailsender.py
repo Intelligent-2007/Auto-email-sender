238d79fb8d126a739b98f3e4a75f96e1940cdf50
Intelligent-2007 <85723710+Intelligent-2007@users.noreply.github.com>
2021-06-17 13:53:16 +0530

    Update autoemailsender.py

--- autoemailsender.py
+++ autoemailsender.py
@@ -1,10 +1,18 @@
-try:
-    import smtplib as smt               
-    import urllib.request                
-    from time import sleep            
+import smtplib as smt               # <----- Email sending
+import urllib.request                 # <------ Internt checks
 
-    # check for internet conncetion
-    def connect(host='http://google.com'):
+from tkinter import *          
+
+root = Tk()
+root.title("Auto Email Bot")
+
+root.geometry("300x250")
+root.maxsize(300,250)
+root.minsize(300,250)
+# root.configure(bg = "red")
+
+
+def connect(host='http://google.com'):
         try:
             urllib.request.urlopen(host)
             return True
@@ -12,7 +20,9 @@ try:
             return False
 
     # check for document made
-    def egmail(file = "Registered_Emails"):
+
+
+def egmail(file = "Registered_Emails"):
         try:
             f = open(file,"r")
             return True
@@ -20,102 +30,195 @@ try:
             return False
 
 
-    # Add email and register email
-    while True:
-        Made = egmail()
+def Made_true():
+    
 
-        if Made == True:                                                                               
-            print("Hello Welocme to the email bot")
-            print("Do you want to send email or add email in your registerd emails")
-            a = input()
-            if a == "Send email":
-                 break                                                                        
-            if a == "Add email":                                                          
-                print("Senders Email:- ")   
-                email = input()
-                print("Senders Password:- ")
-                password = input()
-                with open("Registered_Emails","a") as f :                        
-                    f.write(f"{email} \n ")
-                    f.write(f"{password} \n")
-                    quit()
-   
-        if Made == False:
-            print("Senders Email:- ")   
-            email = input()
-            print("Senders Password:- ")
-            password = input()
-            with open("Registered_Emails","a") as f :
-                f.write(f"{email} \n ")
-                f.write(f"{password} \n")
+
+    
+    def internet_true():
+        welc1.place_forget()
+        Send_email.place_forget()
+        Add_email.place_forget()
+        So1 = Label(root, text = "ENTER THE RECIVERS EMAIL AND MESSAGE")
+        So1.place(relx = 1, x = -150, y = 45, anchor = N)
+
+        Reciver_email = Entry(root, width = 30)
+        Reciver_email.place(relx = 1, x = -150, y = 85, anchor = CENTER)
+        Reciver_email.insert(0,"ENTER RECIVER EMAIL")
+
+        Message1 = Entry(root , width = 30)
+        Message1.place(relx = 1, x = -150,y = 125, anchor = CENTER)
+        Message1.insert(0,"MESSAGE")
+
+
+        def check_for_internet():
+
+            internet = connect()
+            if internet == True:
+                emailsendstart()
+            else:
+                internet_false()
+
+        def internet_false():
+
+            Reciver_email.place_forget()
+            Message1.place_forget()
+            done.place_forget()
+            So1.place_forget()
             
-            print(f"sucessfully regestired {email}")
-            continue
-            break          
-    # Variable defined take user input
+            Itnot = Label(root, text = "You are not connected to internet ").place(relx = 1, x = -150, y = 85, anchor = CENTER)
+
+            def cont2():
+                exit()
+
+           
+            Con2 = Button(root, text = "Cancel", command = cont2).pack(relx = 1, x = -150,y = 125, anchor = CENTER)
+
+    
+        def emailsendstart():
+            done.place_forget()
+            Reciver_email.place_forget()
+            Message1.place_forget()
+            So1.place_forget()
+
+            mail = str(Reciver_email.get())
+            mess = str(Message1.get())
 
 
-    Internet = connect()
-    print("Write the Gmail On which u want to send message")
-    Reciver = input()
-    print("Now write down the messsage which u want to send")
-    Message = input()
-
-    while True:
-        Internet = connect()
-        if Internet == True:
-            print("sucessfully connected to internet")
-            print("from which email do you want to send message")
             file = open('Registered_Emails', "r+")
             details = file.read()
             file.close()
 
-            print(details)    
-            print("Type the email number which u want")
-            number = int(input())
+            info = Label(root, text = details) 
+            info.pack()
+            Num = Label(root, text = "Type The email number which you want to use")
+            Num.pack()
+            Email_num = Entry(root, width = 50)
+            Email_num.pack()
             
-            Number1 = number - 2
-            Number2 = Number1 + number
-            Number3 = Number2 + 1
-            file = open("Registered_Emails", "r+")
-            content = file.readlines()
-            emailnum = content[Number2]
-            passnum = content[Number3]
+
+            def Completep():
+                
+                number = int(Email_num.get())
+                
+
+                Number1 = number - 2
+                Number2 = Number1 + number
+                Number3 = Number2 + 1
+                file = open("Registered_Emails", "r+")
+                content = file.readlines()
+                emailnum = content[Number2]
+                passnum = content[Number3]
             
 
            
-            print("sending email")
-            server = smt.SMTP('SMTP.gmail.com',587)
-            server.starttls()
-            server.login(emailnum,passnum)
-            server.sendmail(emailnum,Reciver,Message)
-            break
-        
-    
-    
-        elif Internet == False:
-        
-            print("You are not conected to internet")
-            print("Type 'continue' to continue or 'cancel' to cancel")
-            go_on = input()
-
-            if go_on == 'cancel':
-                print("SENDING EMAIL CANCELED")
-                break
-            elif go_on == 'continue':
-                print("Your email will be sent when you are connceted to internet")
-                while Internet == True:
-                    Internet = connect()
-                    sleep(1)
-
-except:
-    print("Something went wrong")
-
-# Code written by intelligent2007
-# Reviewed and helped by Gamingfever8250
-             
-             
             
+                server = smt.SMTP('SMTP.gmail.com',587)
+                server.starttls()
+                server.login(emailnum,passnum)
+                server.sendmail(emailnum,mail,mess)
+                info.pack_forget()
+                Email_num.pack_forget()
+                Num.pack_forget()
+                Ok.pack_forget()
+
+                Sent = Label(root, text = "Sucessfully Sent Email!")
+                Sent.place(relx = 1, x = -150,y = 125, anchor = CENTER)
+
+            Ok = Button(root, text = "Submit", command = Completep)
+            Ok.pack()
+
+        done = Button(root, text = "Submit", command = check_for_internet)
+        done.place(relx = 1, x = -165,y = 150)
+
+    def addemail1():
+        welc1.place_forget()
+        Send_email.place_forget()
+        Add_email.place_forget()
+        Senders_email = Entry(root, width = 40) 
+        Senders_email.place(relx = 1, x = -150, y = 85, anchor = CENTER)
+        Senders_email.insert(0, "Type your email")
+
+        Senders_password = Entry(root, width = 40)
+        Senders_password.place(relx = 1, x = -150,y = 125, anchor = CENTER)
+        Senders_password.insert(0,"Type your password")
+    
+        def submit_email():
+            email = Senders_email.get()
+            password = Senders_password.get()
+            with open("Registered_Emails","a") as f :                        
+                f.write(f"{email} \n ")
+                f.write(f"{password} \n")
+            Submit.place_forget()
+            Senders_email.place_forget()
+            Senders_password.place_forget()
+            Made_true()
+
+        Submit = Button(root, width = 20, text = "submit",command = submit_email )
+        Submit.place(relx = 1, x = -165,y = 150)
+        
+    
+    
+    welc1 = Label(root,text = "Welcome To Email Bot")
+    welc1.place(relx = 1, x = -150, y = 45, anchor = N)
+    Send_email = Button(root, text = "Send email", command = internet_true, )
+    Send_email.place(relx = 1, x = -150, y = 85, anchor = CENTER)                                                                              
+    Add_email = Button(root,text = "Add email", command = addemail1)
+    Add_email.place(relx = 1, x = -150,y = 125, anchor = CENTER)
+    
+
+
+
+def addemail():
+
+
+    Senders_email = Entry(root, width = 40) 
+    Senders_email.place(relx = 1, x = -150, y = 85, anchor = CENTER)
+    Senders_email.insert(0, "Type your email")
+
+    Senders_password = Entry(root, width = 40)
+    Senders_password.place(relx = 1, x = -150,y = 125, anchor = CENTER)
+    Senders_password.insert(0,"Type your password")
+    
+    def submit_email():
+        email = Senders_email.get()
+        password = Senders_password.get()
+        with open("Registered_Emails","a") as f :                        
+            f.write(f"{email} \n ")
+            f.write(f"{password} \n")
+        Submit.place_forget()
+        Welcome.place_forget
+        Senders_email.place_forget()
+        Senders_password.place_forget()
+        Welcome.place_forget()
+        Made_true()
+        
+
+    Submit = Button(root, width = 20, text = "submit",command = submit_email )
+    Submit.place(relx = 1, x = -165,y = 150)
+    
+
+
+
+Made = egmail()
+
+if Made == True: 
+    Made_true()
+
+if Made == False:
+    Welcome = Label(root, text = "WELCOME TO EMAIL BOT \n REGISTER YOUR EMAIL ")
+    Welcome.place(relx = 1, x = -150, y = 45, anchor = N)
+    addemail()
+    
+
+
+root.mainloop()
+
+
+
+
+
+    
 
 
 
